b5be3f11200f3a1bc9bb0991ba554caafe0d5b8e
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2022-10-01 14:51:20 +0530

    price commit

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,8 +2,44 @@ const getBtcData = async () => {
     fetch('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD')
    .then(response => response.json())
    .then(data => {
-        console.log(data);
+        document.getElementById("btc").innerHTML = "$"+data['USD'];
+    });
+}
+
+const getEthData = async () => {
+    fetch('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD')
+   .then(response => response.json())
+   .then(data => {
+        document.getElementById("eth").innerHTML = "$"+data['USD'];
+    });
+}
+
+const getAdaData = async () => {
+    fetch('https://min-api.cryptocompare.com/data/price?fsym=ADA&tsyms=USD')
+   .then(response => response.json())
+   .then(data => {
+        document.getElementById("ada").innerHTML = "$"+data['USD'];
+    });
+}
+
+const getSolData = async () => {
+    fetch('https://min-api.cryptocompare.com/data/price?fsym=SOL&tsyms=USD')
+   .then(response => response.json())
+   .then(data => {
+        document.getElementById("sol").innerHTML = "$"+data['USD'];
+    });
+}
+
+const getMaticData = async () => {
+    fetch('https://min-api.cryptocompare.com/data/price?fsym=MATIC&tsyms=USD')
+   .then(response => response.json())
+   .then(data => {
+        document.getElementById("matic").innerHTML = "$"+data['USD'];
     });
 }
 
 getBtcData();
+getEthData();
+getAdaData();
+getSolData();
+getMaticData();

426388d748a63b86b1d05d7eedd93b22add9b464
Akshay Anand <86616444+Akshay2931@users.noreply.github.com>
2022-10-01 15:41:20 +0530

    wallet commit

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -43,3 +43,17 @@ getEthData();
 getAdaData();
 getSolData();
 getMaticData();
+
+async function connect() {
+    if (window.ethereum) {
+       await window.ethereum.request({ method: "eth_requestAccounts" });
+       window.web3 = new Web3(window.ethereum);
+       const account = web3.eth.accounts;
+       //Get the current MetaMask selected/active wallet
+       const walletAddress = account.givenProvider.selectedAddress;
+       console.log(`Wallet: ${walletAddress}`);
+    
+    } else {
+     console.log("No wallet");
+    }
+  }
